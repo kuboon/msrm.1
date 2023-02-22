51daf5d3dd8b5bb4273e96221610e7f682bbbee7
Ohkubo KOHEI <ohkubo@heartrails.com>
2023-02-23 00:05:51 +0900

    remove

---
+++ webauthn-skeleton/app.ts
@@ -0,0 +1,50 @@
+import { opine, json, serveStatic, OpineSession, HTTPSOptions, HTTPOptions, WrappedRequest, ConnInfo} from "./deps.ts";
+// import { config } from "./config.ts";
+import defaultRoutes from "./routes/default.ts";
+import tokenRoutes from "./routes/token.ts";
+import webauthnRoutes from "./routes/webauthn.ts";
+
+const app = opine();
+
+// Enable sessions
+new OpineSession(app);
+
+// Enable json body parser
+app.use(json());
+app.use(serveStatic("./webauthn-skeleton/public/static"));
+
+// Set up routes
+app.use("/", defaultRoutes);
+app.use("/token", tokenRoutes);
+app.use("/webauthn", webauthnRoutes);
+
+// Set up config
+// let appConfig: HTTPSOptions | HTTPOptions;
+
+// // "Development" HTTPS
+// if (config.mode === "development") {
+// 	appConfig = {
+// 		port: parseInt(config.port, 10),
+// 		certFile: "./keys/cert.pem",
+// 		keyFile: "./keys/key.pem"
+// 	}
+
+// // "Production" HTTP - (for use behind https proxy)
+// } else {
+// 	appConfig = {
+// 		port: parseInt(config.port, 10)
+// 	};
+// }
+
+// Start server
+// app.listen(appConfig,
+// 	() => console.log("server has started on https://localhost:"+config.port+" 🚀"),
+// );
+
+export async function handler(request: Request, connInfo: ConnInfo){
+	const opineRequest = new WrappedRequest(request, connInfo);
+	app(opineRequest);
+	const res = await opineRequest.finalResponse;
+	request.session = opineRequest.session;
+	return res;
+}
---
+++ webauthn-skeleton/deps.ts
@@ -0,0 +1,16 @@
+// Deno third-party
+import { opine, json, serveStatic, Router, HTTPSOptions, HTTPOptions } from "https://deno.land/x/opine@2.2.0/mod.ts";
+import type { ConnInfo } from "https://deno.land/x/opine@2.2.0/deps.ts";
+export { opine, json, serveStatic, Router };
+export type { HTTPSOptions, HTTPOptions, ConnInfo };
+export { WrappedRequest } from "https://deno.land/x/opine@2.2.0/src/request.ts"
+
+import { FileDB, Document } from "https://deno.land/x/filedb@0.0.6/mod.ts";
+export { FileDB };
+export type { Document };
+
+export { OpineSession } from "https://deno.land/x/sessions@v1.5.4/mod.ts";
+export { Fido2Lib } from "https://deno.land/x/fido2@3.2.4/dist/main.js";
+
+// Third party
+export { base64 } from "https://deno.land/x/b64@1.0.20/src/base64.js";
